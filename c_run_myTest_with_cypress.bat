066abb482debbf535a01d61630125eee71802c57
Didier_Defrance <didier@d-defrance.fr>
2025-02-27 18:16:41 +0100

    s

--- c_run_myTest_with_cypress.bat
+++ c_run_myTest_with_cypress.bat
@@ -1,4 +1,6 @@
-npx cypress run --spec "cypress/e2e/myTest.spec.cy.js" --browser chrome > test_report.txt 
+REM npx cypress run --spec "cypress/e2e/myTest.spec.cy.js" --browser chrome > test_report.txt
+REM npx cypress run --spec "cypress/e2e/myTest.spec.cy.js" --browser electron > test_report.txt 
+npx cypress run --spec "cypress/e2e/myTest.spec.cy.js" > test_report.txt
 REM option headless par défaut pour ne pas montrer le navigateur sous controle
 REM si pas option --browser chrome alors navigateur electron par defaut
 REM ajouter dans  e2e:{ ...} (à la fin) , "video" : false dans cypress.config.js pour désactiver l'enregistrement video
