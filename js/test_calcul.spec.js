d90c7bac1ffea841c433e89d1be7f8ac0833999c
Didier_Defrance <didier@d-defrance.fr>
2025-07-31 13:30:52 +0200

    s

--- js/test_calcul.spec.js
+++ js/test_calcul.spec.js
@@ -1,3 +1,20 @@
+//import { calculerOp } from "./calcul_with_export.js
+
+//copy of function to test (without import/export) in this basic/project:
+function calculerOp(op,a,b){ 
+    a = Number(a);
+	b = Number(b);
+	var res = 0;
+	if(op == '+'){
+		 res = a+b; 
+	}else if(op == '*'){
+		 res=a*b;
+	}else{
+	   res =eval ("a"+op+"b"); //eval("a-b") ou eval("a/b") ou ...
+	}
+	return res;
+}
+
 describe('calculs', () => {
   it('5+6==11 en direct', () => {
 	  res=5+6
@@ -5,7 +22,7 @@ describe('calculs', () => {
   })
   
   it('5+6==11 via fonction calculerOp de calcul.js', () => {
-	  res=calculerOp(5,6)
+	  res=calculerOp('+',5,6)
 	  expect(res).toEqual(11)
   })
 })
