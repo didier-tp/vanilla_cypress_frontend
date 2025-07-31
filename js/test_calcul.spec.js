6bc7b90386d7974ed8979f1a50cad7ee8698c7c7
Didier_Defrance <didier@d-defrance.fr>
2025-07-31 12:27:46 +0200

    s

--- js/test_calcul.spec.js
+++ js/test_calcul.spec.js
@@ -1,6 +1,11 @@
 describe('calculs', () => {
-  it('5+6==11', () => {
+  it('5+6==11 en direct', () => {
 	  res=5+6
+	  expect(res).toEqual(11)
+  })
+  
+  it('5+6==11 via fonction calculerOp de calcul.js', () => {
+	  res=calculerOp(5,6)
 	  expect(res).toEqual(11)
   })
 })
